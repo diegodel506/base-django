138acb5d8678887df76ffcbf43df6d91e002d61a
DiegoJD <50335865+diegodel506@users.noreply.github.com>
2024-01-22 12:04:15 -0600

    updated

--- base/settings.py
+++ base/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-wpn^qd@n787zo&&#t!!)+r4rh-q_e%n=xddx#=pzs_4+-rib2u'
-SECRET_KEY = os.environ.get("SECRET_KEY", default="your secret key")
+SECRET_KEY = "django-insecure-wpn^qd@n787zo&&#t!!)+r4rh-q_e%n=xddx#=pzs_4+-rib2u"
+# SECRET_KEY = os.environ.get("SECRET_KEY", default="your secret key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
